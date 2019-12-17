1b22de3dadf464680be68e720dd77c64fb7ad480
sambhu paul <sambhu.paul87@gmail.com>
2019-12-17 12:40:31 -0500

    initial api commit

---
+++ DatingWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingWebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ApplicationDataContext _context;
+
+        public ValuesController(ApplicationDataContext context)
+        {
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var values = await _context.Values.ToListAsync();
+                return Ok(values);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(HttpContext.Response.StatusCode);
+            }
+            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+                if(value == null)
+                {
+                    return NoContent();
+                }
+                return Ok(value);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingWebApi/Data/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using DatingWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingWebApi.Data
+{
+    public class ApplicationDataContext: DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options): base(options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
